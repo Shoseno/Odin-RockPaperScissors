5d2aade343236ff35fc512eaeffc8fca8ceb6340
Shoseno <129028927+Shoseno@users.noreply.github.com>
2023-11-23 18:06:18 -0500

    Add playRound function and fixed getPlayerChoice not saving playChoice assignment

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -2,6 +2,9 @@
 
 
 let compChoice;
+let playerChoiceCheck;
+let playChoice;
+let playInput;
 
 function getComputerChoice() {
     let compChoiceRandomNum = Math.floor(Math.random() * 3) + 1;
@@ -12,20 +15,33 @@ function getComputerChoice() {
     } else if (compChoiceRandomNum === 3) {
         compChoice = 'Rock';
     }
-
+    console.log(compChoice);
 }
-let playerChoiceCheck;
-let playChoice;
-let playInput;
+
 
 function getPlayerChoice() {
     let playInput = prompt('Please type Rock,Paper, or Scissors');
-    let playChoice = (playInput.charAt(0).toUpperCase()) + (playInput.slice(1).toLowerCase());
+    playChoice = (playInput.charAt(0).toUpperCase()) + (playInput.slice(1).toLowerCase());
     if (playChoice === 'Rock' || playChoice === 'Paper' || playChoice ==='Scissors') {
         playerChoiceCheck = 'yes';
     } else {
         playerChoiceCheck = 'no';
-        alert('Please enter "Rock", "Paper" or "Scissors"');
+        alert('PLEASE ENTER "Rock", "Paper" or "Scissors" you entered ' + playChoice);
         getPlayerChoice();
+    }
+    console.log(playChoice);
+}
+
+function playRound() {
+    getComputerChoice();
+    getPlayerChoice();
+    if (playChoice === compChoice) {
+        alert('you TIED, you and the computer chose ' + playChoice);
+    } else if ((playChoice === 'Rock' && compChoice === 'Scissors')
+              || (playChoice === 'Paper' && compChoice === 'Rock')
+              || (playChoice === 'Scissors' && compChoice === 'Paper')) {
+        alert('you WIN, ' + playChoice + ' beats ' + compChoice + '!');
+    } else {
+        alert('you LOSE, ' + playChoice + ' loses to ' + compChoice + '!'); 
     }
 }
